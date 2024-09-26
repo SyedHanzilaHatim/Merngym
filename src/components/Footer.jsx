fafd87b5e8d4344809b2082e2ac16e84a8607b59
SyedHanzilaHatim <syedhanzilahatim55@gmail.com>
2024-09-26 21:36:29 +0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed And Developed By Syed Hanzila Hatim.
+        All CopyRights Are Reserved 2024
+    </footer>
+  )
+}
+
+export default Footer
